cffd8d8b3173b40d595ff96bcfefd5227a50965d
Lorenza SF (Lola) <85140015+lorenzasf@users.noreply.github.com>
2022-11-19 21:50:51 -0300

    Update urls.py

--- core/tasks/urls.py
+++ core/tasks/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
-urlpatterns = []
-
+urlpatterns = [
+    path('get/', views.getTask),
+    path('post/', views.postTask),
+    path('put/<int:pk>/', views.putTask),
+    path('delete/<int:pk>/', views.deleteTask)
+]
