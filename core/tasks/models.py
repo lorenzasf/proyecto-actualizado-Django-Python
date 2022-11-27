3814ae49abc624d47a03245394551bffc895096a
lorenzasf <lorenza.argentina@gmail.com>
2022-11-26 23:32:03 -0300

    Agregué configuraciones de PostgreSQL para testear conexión

--- core/tasks/models.py
+++ core/tasks/models.py
@@ -1,8 +1,34 @@
+# from django.db import models
+# from authentication.models import CustomUser
+
+# # Create your models here.
+# class Priority(models.Model):
+#     name = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.name
+
+# class Task(models.Model):
+#     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=150, null=False)
+#     description = models.TextField(null=True, blank=True)
+#     is_completed = models.BooleanField(default=False)
+#     priority = models.ForeignKey(Priority, on_delete=models.PROTECT)
+
+#     def __str__(self):
+#         return f"{self.title} {self.user}"
+
 from django.db import models
 from authentication.models import CustomUser
 
 # Create your models here.
 class Priority(models.Model):
+    """
+        Modelo para Catálogo de Prioridades.
+        1: Baja
+        2: Media
+        3: Alta
+    """
     name = models.CharField(max_length=100)
 
     def __str__(self):
--- core/tasks/views.py
+++ core/tasks/views.py
@@ -39,6 +39,37 @@
 #     task.delete()
    # return Response('Task eliminated')
 
+# from django.shortcuts import render
+# from rest_framework import generics
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+
+# from .models import Task
+# from .serializers import TaskSerializer
+
+# # Create your views here.
+# class GetAllTasksView(generics.ListAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+
+# class GetTasksByUserIDView(generics.ListAPIView):
+#     serializer_class = TaskSerializer
+
+#     def get_queryset(self):
+#         user_id = self.request.query_params.get('user_id')
+#         queryset = Task.objects.filter(user = user_id)
+#         return queryset
+
+# @api_view(['GET'])
+# def task_detail(request, task_id):
+#     try:
+#         task = Task.objects.get(pk=task_id)
+#         serializer = TaskSerializer(task)
+#         return Response(serializer.data, status = 200)
+#     except:
+
+#         return Response({"msg": "No existe ninguna tarea con ese ID"}, status = 404)
+
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
@@ -49,6 +80,9 @@ from .serializers import TaskSerializer
 
 # Create your views here.
 class GetAllTasksView(generics.ListAPIView):
+    """
+        Esta vista nos retorna todas las tareas registradas. Sin filtro.
+    """
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
