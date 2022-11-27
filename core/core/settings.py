e9fb4b00128dfe5e1ec344d9c082adf74309748c
lorenzasf <lorenza.argentina@gmail.com>
2022-11-26 23:18:46 -0300

    Agregué configuraciones de PostgreSQL para testear conexión

--- core/core/settings.py
+++ core/core/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taskapp',
+        'USER': 'postgres',
+        'PASSWORD': '147369',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
